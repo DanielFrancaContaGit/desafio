b18beb6aa92f4c13442abdfdb98a3b133a8ff34e
Daniel França <daniel.pe.15y15@gmail.com>
2020-01-08 18:58:11 -0300

    primeiro commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(){
+    return(
+        <section>
+            <div>Oi mundo</div>
+        </section>
+    )
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
